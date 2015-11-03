217e52b9f8bb508e868bbf353d8719f95c5afaae
Sparc0 <sparco@hackernet.se>
2015-11-03 14:13:21 +0100

    Puppet style.
    Better style on code,  made it more community friendly.

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,16 +1,15 @@
 # Ensures that environment-modules is installed.
 #
 class modules::install {
-        Package {
-          ensure => installed
-        }
-        case $::osfamily {
-          'Debian', 'Ubuntu': {
-            package { 'environment-modules': }
-           }
-           default: {
-             fail("${::operatingsystem} not supported.")
-           }
-        }       
-               
+  Package {
+    ensure => installed
+  }
+  case $::osfamily {
+    'Debian', 'Ubuntu': {
+      package { 'environment-modules': }
+    }
+    default: {
+      fail("${::operatingsystem} not supported.")
+    }
+  }
 }
